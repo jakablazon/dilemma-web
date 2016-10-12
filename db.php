676a20c3200a6aad18a5cbd26954482032830e72
Jure Cindro <jure@elpy.io>
2016-10-12 17:51:09 +0200

    Add: heroku db connection data

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 
-$servername = "localhost";
-$username = "blazon";
-$password = "jaka";
-$dbname = "blazon";
+$servername = "us-cdbr-iron-east-04.cleardb.net";
+$username = "b5a41126f36259";
+$password = "541de83f";
+$dbname = "heroku_52a0a6fd9c43d3c";
