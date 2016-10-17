5e06bc68fb8181ef1481698839cdf1bef4a1edcd
Jure Cindro <jure@elpy.io>
2016-10-17 15:55:03 +0200

    Removed old files and created ruby project

--- db.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$servername = "us-cdbr-iron-east-04.cleardb.net";
-$username = "b5a41126f36259";
-$password = "541de83f";
-$dbname = "heroku_52a0a6fd9c43d3c";
--- helpers.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function increment($conn, $id, $column)
-{
-    mysqli_query($conn,
-        "UPDATE dilemma
-        SET $column=$column+1
-        WHERE questionID=$id;");
-}
